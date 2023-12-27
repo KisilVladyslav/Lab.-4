ce127b73269b5110ed8f4d3ca981940b2b1fbd68
Kisil_Vladyslav <kisilvladyslav@gmail.com>
2023-12-27 15:42:55 +0200

    Dockerfile

---
+++ МРПЗ Лаб.№3/Ls.cli2/Program.cs
@@ -0,0 +1,48 @@
+﻿using L3.Domain;
+
+var taskController = new TaskController();
+
+
+var positiveTaskType = "Positive";
+var negativeTaskType = "Negative";
+
+taskController.AddTaskCategory(positiveTaskType);
+taskController.AddTaskCategory(negativeTaskType);
+
+int index = 0;
+
+var Tasks = Enumerable
+    .Range(0, 10)
+    .Select(x => new L3.Domain.Task
+    {
+        Title = $"Title: {index}",
+        Description = $"Title: {index++}",
+        Category = positiveTaskType,
+    })
+    .ToList();
+
+Tasks.AddRange(Enumerable
+    .Range(0, 10)
+    .Select(x => new L3.Domain.Task
+    {
+        Title = $"Title: {index}",
+        Description = $"Title: {index++}",
+        Category = negativeTaskType,
+        IsFavourite = true,
+    })
+    .ToList());
+
+foreach (var task in Tasks)
+{
+    taskController.AddTask(task);
+}
+
+foreach (var task in taskController.GetAllFavoriteTasksOrderedDescending())
+{
+    Console.WriteLine(task);
+}
+
+foreach (var task in taskController.GetAllTasksByCategoryOrderedDescending(positiveTaskType))
+{
+    Console.WriteLine(task);
+}
---
+++ МРПЗ Лаб.№3/МРПЗ Лаб.№3/Task.cs
@@ -0,0 +1,26 @@
+﻿namespace L3.Domain {
+
+    public class Task
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public bool IsFavourite { get; set; }
+
+        public Task(string title, string description, string category, bool isFavorite = false)
+        {
+            Title = title;
+            Description = description;
+            Category = category;
+            IsFavourite = isFavorite;
+        }
+        public Task() { }
+
+        public bool IsValid()
+        {
+            return !(string.IsNullOrWhiteSpace(Title) ||
+                string.IsNullOrWhiteSpace(Description) ||
+                string.IsNullOrWhiteSpace(Category));
+        }
+    }
+}
